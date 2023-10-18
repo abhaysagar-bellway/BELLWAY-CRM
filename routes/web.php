a645c3aca461252379ada8855df59f6426df2cfc
Abhay <abhaysagar818@gmail.com>
2023-10-18 14:22:46 +0530

    CRM

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
+Route::get('/', function () {
+    return view('admin.login');
+});
 
 // Route::middleware([
 //     'auth:sanctum',
