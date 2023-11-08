72ae59e37025f69106d5f59bf0f94e5f456d6ec0
Abhay <abhaysagar818@gmail.com>
2023-11-08 18:22:23 +0530

    role_id fixed

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -15,25 +15,25 @@ class RoleController extends Controller
         $userData =  User::where('role_id', '!=', 1)->get(); 
         $data = DB::table('users')
         ->join('roles', 'users.role_id', '=', 'roles.id')
-        ->select('users.first_name','users.last_name', 'roles.rolename', 'roles.role_permission')
+        ->select('users.first_name','users.last_name', 'roles.rolename', 'roles.role_permission','roles.id')
         ->get();
 
-        return view('admin.role',array('role' => $role, 'userData' => $userData));
+        return view('admin.role',array('role' => $role, 'data' => $data));
     }
 
     public function addrole(Request $request)
     {
         $validatedData = $request->validate([
-            'rolename' => 'required|min:4',
+            'rolename' => 'required',
             'role_permission ' => 'required',
          ]);
 
          $role = new Role;
-         $role->rolename = $request->rolename;   
+         $role->rolename = $request->rolename; 
+         $User = new User;
+         $User->first_name = $request->first_name;
+         $User->last_name = $request->last_name;
          $role->role_permission =  implode(" ,",$request->role_permission);
-
-        
-         
          $role->save();
          return redirect()->back()->with('status','Role Data Add Successfully!');
 
